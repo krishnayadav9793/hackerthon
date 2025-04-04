85db27d231ec16c7221d3efcd9cb0460e65a63b6
krishnayadav9793 <karanyadav21398@gmail.com>
2025-04-04 23:38:39 +0530

    commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express =require('express')
+const colors =require('colors')
+const dotenv =require('dotenv').config()
+const connectDB=require('./config/db')
+
+connectDB()
+const {errorHandler}=require("./middlwware/errorMiddleware")
+const port =process.env.PORT || 5000
+
+const app= express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals',require('./routes/goalRoutes'))
+app.use('/api/users',require('./routes/userRoutes'))
+app.use(errorHandler)
+
+
+app.listen(port,()=>{
+    console.log(`server started on port ${port}`)
+})
